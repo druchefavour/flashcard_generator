c3836353103b669f53b276032f57fcec752141a3
druchefavour <druchefavour@gmail.com>
2017-01-21 18:39:16 -0600

    commit full BasicCard constructor

--- cardUser.js
+++ cardUser.js
@@ -2,4 +2,14 @@
 function BasicCard (front, back) {
 	this.front = front;
 	this.back = back;
-}
+
+	// method to display the full text
+	this.printFullText = function () {
+		console.log("Full Text: " + this.front + " " + this.back );
+		}
+		this.printFullText();
+	}
+
+// Create a new BasicCard Player
+
+var newBasicCardPlayer = new BasicCard ("Who was the first president of the United States?", "George Washington");
