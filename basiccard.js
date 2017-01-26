ecbd660d90584404107e562d00faa79271963819
druchefavour <druchefavour@gmail.com>
2017-01-26 10:39:34 -0600

    commit application that works whether the word new is used in the constructor or not

--- basiccard.js
+++ basiccard.js
@@ -1,6 +1,10 @@
 // Create Basic Flascard function accepting front and back arguments
 
 var BasicFlashCard = function (frontText, backText) {
+	//Write constructors such that users can call them with or without the new keyword
+	if (!(this instanceof BasicFlashCard)) {
+		return new BasicFlashCard(frontText, backText);
+	}
 
 	//Create properties to display frontext and backtext
 	this.frontText = frontText;
@@ -15,3 +19,7 @@ var BasicFlashCard = function (frontText, backText) {
 // Create BasicCard constructor
 
 var newBasicFlashCard = new BasicFlashCard("Who was the first president of the United States?", "George Washington");
+var anotherBasicFlashCard = BasicFlashCard("Who was the first president of the United States?", "George Washington");
+
+console.log(newBasicFlashCard.backText);
+console.log(anotherBasicFlashCard.backText);
