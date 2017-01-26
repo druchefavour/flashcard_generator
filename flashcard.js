0bba49c452b99dc1f22d4e523833da97a8d4fabe
druchefavour <druchefavour@gmail.com>
2017-01-26 13:01:39 -0600

    commit switch function to pass different flashcards

--- flashcard.js
+++ flashcard.js
@@ -7,6 +7,26 @@ var ClozeCard = require("./clozecard.js");
 var newBasicCard = new BasicCard("Who was the first President of the United States", "George Washington")
 var newClozeCard = new ClozeCard ("---", "was the first president of the United States", "George Washington");
 
+// Create script to pass flashcards
+var passFlashCard = function () {
+	inquirer.prompt({
+		name: "action",
+		type: "display",
+		message: "What would you do?",
+		choices: ["Pass a Basic flash Card", "Pass a Cloze flash Card"]
+	}).then(function(answer){
+		switch (answer.action) {
+			case "Pass a Basic flash Card":
+			passBasicCard();
+			break; 
+
+			case "Pass a Cloze flash card":
+			passClozeCard();
+			break;
+		}
+	});
+};
+
 //This will print everything in exports
 console.log("--------------------------");
 console.log("THE FLASH CARDS");
