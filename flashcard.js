f2f9e1ac0798265cb6552cbaff7ba32fefd3c17c
druchefavour <druchefavour@gmail.com>
2017-01-26 12:44:17 -0600

    commit to create basic and cloze flashcard constructors

--- flashcard.js
+++ flashcard.js
@@ -1,11 +1,15 @@
 // Using the require keyword lets us access all of exports in the basiccard.js file
 
+var BasicCard = require("./basiccard.js");
 var ClozeCard = require("./clozecard.js");
 
-var oneClozeFlashCard = new ClozeCard ("---", "was the first president of the United States", "George Washington");
+// Create Basic Flashcard constructor
+var newBasicCard = new BasicCard("Who was the first President of the United States", "George Washington")
+var newClozeCard = new ClozeCard ("---", "was the first president of the United States", "George Washington");
 
 //This will print everything in exports
 console.log("--------------------------");
-console.log("ALL THE STUFF I NEED");
-console.log(oneClozeFlashCard.displayFullText());
+console.log("THE FLASH CARDS");
+console.log(newBasicCard.getBackText());
+console.log(newClozeCard.displayFullText());
 console.log("--------------------------");
